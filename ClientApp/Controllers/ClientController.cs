5719c9e5f15e03d2d4efad3f790c9b26886850e3
eluraschi <Eduardoluraschi@gmail.com>
2021-11-22 15:55:45 -0300

    ok

--- ClientApp/Controllers/ClientController.cs
+++ ClientApp/Controllers/ClientController.cs
@@ -14,7 +14,6 @@ namespace ClientApp.Controllers
 {
     public class ClientController : Controller
     {
-        // GET: ClientController
         public ActionResult NewFinalClient()
         {
             ViewBag.colClientTypeSelect = GetSelectClientType();
@@ -59,68 +58,7 @@ namespace ClientApp.Controllers
             return RedirectToAction("NewCompany  ");
         }
 
-        // GET: ClientController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: ClientController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ClientController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ClientController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ClientController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ClientController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
 
         #region AUX
         public List<SelectListItem> GetSelectClientType()
