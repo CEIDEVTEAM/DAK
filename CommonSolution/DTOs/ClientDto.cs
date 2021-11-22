1a855000d00a0b0404f60582af2ce057fbe9a90d
eluraschi <Eduardoluraschi@gmail.com>
2021-11-21 23:52:12 -0300

    AddClient

--- CommonSolution/DTOs/ClientDto.cs
+++ CommonSolution/DTOs/ClientDto.cs
@@ -1,6 +1,8 @@
 ﻿using CommonSolution.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +12,25 @@ namespace CommonSolution.DTOs
     public abstract class ClientDto : IDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El grupo de cliente es requerido")]
+        [DisplayName("Grupo de cliente")]
         public string BillingType { get; set; }
+
+        [StringLength(15, ErrorMessage = "El teléfono no puede superar los 15 caracteres")]
+        [Required(ErrorMessage = "El teléfono es requerido")]
+        [DisplayName("Teléfono")]
         public string PhoneNumber { get; set; }
+
+        [StringLength(100, ErrorMessage = "La dirección no puede superar los 100 caracteres")]
+        [Required(ErrorMessage = "La dirección es requerida")]
+        [DisplayName("Dirección")]
         public string Address { get; set; }
+
+        [StringLength(50, ErrorMessage = "El eMail no puede superar los 50 caracteres")]
+        [Required(ErrorMessage = "El eMail es requerida")]
+        [EmailAddress(ErrorMessage = "El eMail no es válido")]
+        [DisplayName("Email")]
         public string EMail { get; set; }
-
-
     }
 }
--- CommonSolution/DTOs/FinalClientDto.cs
+++ CommonSolution/DTOs/FinalClientDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,20 @@ namespace CommonSolution.DTOs
     public class FinalClientDto : ClientDto
     {
         public int IdClient { get; set; }
+
+        [StringLength(15, ErrorMessage = "El documento no puede superar los 15 caracteres")]
+        [Required(ErrorMessage = "El documento es requerido")]
+        [DisplayName("Documento")]
         public string DocumentNumber { get; set; }
+
+        [StringLength(20, ErrorMessage = "El nombre no puede superar los 20 caracteres")]
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [DisplayName("Nombre")]
         public string Name { get; set; }
+
+        [StringLength(20, ErrorMessage = "El apellido no puede superar los 20 caracteres")]
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [DisplayName("Apellido")]
         public string LastName { get; set; }
     }
 }
