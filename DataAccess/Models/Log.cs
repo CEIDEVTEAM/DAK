1e081484a3984ddcf9408b262d117d6546aa2fba
eluraschi <Eduardoluraschi@gmail.com>
2021-11-25 22:15:46 -0300

    update database

---
+++ DataAccess/Models/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataAccess.Models
+{
+    public partial class Log
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public DateTime DtLog { get; set; }
+        public string UserDocument { get; set; }
+
+        public virtual User UserDocumentNavigation { get; set; }
+    }
+}
---
+++ DataAccess/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataAccess.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Logs = new HashSet<Log>();
+        }
+
+        public string UserDocument { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string UserType { get; set; }
+
+        public virtual ICollection<Log> Logs { get; set; }
+    }
+}
